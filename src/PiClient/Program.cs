dfccb8f1b92e82d8d1fe50a668f3024dc4ec6279
Ubuntu <ubuntu@ubuntu.lan>
2021-11-24 19:35:42 +0000

    new projects

---
+++ src/PiClient/Program.cs
@@ -0,0 +1,10 @@
+using PiClient;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
